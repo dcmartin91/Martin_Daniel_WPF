0c5cf8415b4afd7c5344bfbbbef22e14f804aa85
dcmartin91 <dcmartin1@gmail.com>
2015-04-16 23:33:45 -0400

    write intro

--- Martin_Daniel_Expressions_Assignment/JavaScript.js
+++ Martin_Daniel_Expressions_Assignment/JavaScript.js
@@ -1,4 +1,10 @@
-// Daniel Martin
-// April 14,2015
-//SDI Sec 02
-//Expressions assignment//
+// Daniel Martin//
+// April 14,2015//
+// SDI Sec 02//
+// Expressions assignment//
+
+
+
+
+
+//Calculator for determining the amount of money you would end up if you invested money that is set to grow over that period//
